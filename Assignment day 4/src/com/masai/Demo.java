67bd440b598bab1c7610f668ed401cad8d17915b
Anubhav Mishra <omanubhavmishra@gmail.com>
2022-05-19 21:24:55 +0530

    assignment day4

---
+++ Assignment day 4/src/com/masai/Demo.java
@@ -0,0 +1,31 @@
+package com.masai;
+
+public class Demo {
+   Demo(){
+	   System.out.println("This is the Demo() constructor");
+   }
+   
+   Demo(String s)
+   {
+	   System.out.println("This is the Demo(String s) constructor");
+   }
+   
+   Demo(int i)
+   {
+	   System.out.println("This is the Demo(int i) constructor");
+   }
+   
+   Demo(float f)
+   {
+	   System.out.println("This is the Demo(float f) constructor");
+   }
+   
+   public static void main(String[] args) {
+
+	   Demo d1 = new Demo(10.5f);
+	   
+	   
+	   
+	   
+	}
+}
