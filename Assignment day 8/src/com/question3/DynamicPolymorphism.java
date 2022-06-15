1acdd5581df928f4e8f5cca2907f98b50818ea8b
Anubhav Mishra <omanubhavmishra@gmail.com>
2022-06-15 22:24:38 +0530

    added

--- Assignment day 8/src/com/question3/DynamicPolymorphism.java
+++ Assignment day 8/src/com/question3/DynamicPolymorphism.java
@@ -2,4 +2,6 @@ package com.question3;
 
 public class DynamicPolymorphism {
 
+	//Dynamic Polymorphism is a type of polymorphism that collects the information for calling a method at runtime.
+	//when we use "Override" the class in its extends class 
 }
